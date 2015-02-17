1f4b01489e78529bebaff7ebfbe7f724fe513fd7
Tony Beckham <tony.beckham@gmail.com>
2015-02-17 14:52:57 -0800

    Remove unused/needed functions. Write number of hosts as an environment variable key=value format to a properties file to be used by all jobs in the pipeline.

--- topology_parser.py
+++ topology_parser.py
@@ -3,32 +3,18 @@
 # Author: Tony Beckham tony.beckham@hp.com
 #
 
-import json
 import os
 import yaml
 
 
-def write_json_environment():
-    environment_dict = yaml.load(open(environment_decriptor).read())
-    filename = 'environment.json'
-    with open(filename, 'w') as env_json:
-        env_json.write(json.dumps(environment_dict, indent=4,
-                                  sort_keys=True, separators=(',', ': ')))
-    return
+environment_descriptor= os.getenv('TOPOLOGY')
+topo_dict = yaml.load(environment_descriptor)
+num_hosts=len(topo_dict['machine-topology'])
+f = open('build.properties', 'w')
+f.write("NUM_HOSTS="+ str(num_hosts))
 
-def determine_number_of_hosts(environment_yml):
-    topo_dict = yaml.load(open(environment_yml).read())
-    num_host = len(topo_dict['machine-topology'])
-    return num_host
-
-
-environment_decriptor = os.getenv('TOPOLOGY')
-os.getenv('NUM_HOSTS', determine_number_of_hosts(environment_decriptor))
-
-topo_dict = yaml.load(open(environment_decriptor).read())
-
-print "This is the determined topology"
+print "\nThis is the determined topology"
 for i, v in enumerate(topo_dict['machine-topology']):
     print v['cloud-components']
 
-print "\nWe need " + str(determine_number_of_hosts(environment_decriptor)) + " hosts\n"
+print "\nReserving " + str(num_hosts) + " hosts\n"
