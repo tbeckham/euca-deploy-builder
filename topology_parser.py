393841f5b6dc9911478e302b17662c7f1106e273
Tony Beckham <tony.beckham@gmail.com>
2015-02-17 13:28:55 -0800

    Begin jenkins integration

--- topology_parser.py
+++ topology_parser.py
@@ -9,7 +9,7 @@ import yaml
 
 
 def write_json_environment():
-    environment_dict = yaml.load(open(environment_file).read())
+    environment_dict = yaml.load(open(environment_decriptor).read())
     filename = 'environment.json'
     with open(filename, 'w') as env_json:
         env_json.write(json.dumps(environment_dict, indent=4,
@@ -21,11 +21,14 @@ def determine_number_of_hosts(environment_yml):
     num_host = len(topo_dict['machine-topology'])
     return num_host
 
-environment_file = "environment.yml"
-topo_dict = yaml.load(open(environment_file).read())
+
+environment_decriptor = os.getenv('TOPOLOGY')
+os.getenv('NUM_HOSTS', determine_number_of_hosts())
+
+topo_dict = yaml.load(open(environment_decriptor).read())
 
 print "This is the determined topology"
 for i, v in enumerate(topo_dict['machine-topology']):
     print v['cloud-components']
 
-print "\nWe need " + str(determine_number_of_hosts(environment_file)) + " hosts\n"
+print "\nWe need " + str(determine_number_of_hosts(environment_decriptor)) + " hosts\n"
