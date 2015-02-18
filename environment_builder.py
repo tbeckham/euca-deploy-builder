dc038426517a296e85527131f5145ea31d7f2397
Tony Beckham <tony.beckham@gmail.com>
2015-02-18 13:37:14 -0800

    Just need to get some stuff checked in. Mostly making top parser a utility class for diferent parsing actions on topology format.

--- environment_builder.py
+++ environment_builder.py
@@ -6,12 +6,33 @@
 import json
 import os
 import yaml
+import topology_parser
+
+'''
+Jenkins ENV Vars
+'''
+# job_id = os.getenv('JOB_ID')
+# print "DEPLOY JOB ID IS: " + job_id
+# install_type = os.getenv('SOURCE_OR_PACAKGE_BUILD')
+# frontend = os.getenv('MACHINE_1')
+# public_ips = os.getenv('PUBLIC_IPS')
+# euca_source = os.getenv('EUCA_SOURCE')
+# private_ips = os.getenv('PRIVATE_IPS')
+# source_branch = os.getenv('SOURCE_BRANCH')
+# euca2ools_version = os.getenv('EUCA2OOLS_VERSION')
+# hypervisor = os.getenv('HYPERVISOR')
+# network_mode = os.getenv('NETWORK')
+# block_storage_mode = os.getenv('BLOCK_STORAGE')
+# object_storage_mode = os.getenv('OBJECT_STORAGE')
 
 network_mode = "EDGE"
 default_cluster_name = "one"
-client_file = "user-topo.yml"
+client_file = "client.yml"
 environment_file = "environment.yml"
 user_dict = yaml.load(open(client_file).read())
+
+#simulate jenkins env vars
+euca_source = "testing"
 
 def write_json_environment():
     environment_dict = yaml.load(open(environment_file).read())
@@ -29,7 +50,6 @@ def merge(user, default):
             else:
                 user[k] = merge(user[k],v)
     return user
-
 
 default = {'description': 'Eucalyptus CI Testing',
            'thrift': {'version': '0.9.1'}}
@@ -51,13 +71,26 @@ eucalyptus = {"default-img-url": "http://images.walrus.cloud.qa1.eucalyptus-syst
               "system-properties": {'cloudformation.url_domain_whitelist': '*s3.amazonaws.com,*qa1.eucalyptus-systems.com'}
 }
 
-
 default["default_attributes"] = {"eucalyptus": eucalyptus}
 
+repostitory_mapping = {'testing': {
+    'eucalyptus-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/eucalyptus-devel/centos/6/x86_64/',
+    'enterprise-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/enterprise-devel/centos/6/x86_64/',
+    'euca2ools-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/euca2ools-devel/centos/6/x86_64/'},
+                       'maint/4.0/testing': {
+                           'eucalyptus-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/eucalyptus-4.0/centos/6/x86_64/',
+                           'enterprise-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/enterprise-4.0/centos/6/x86_64/',
+                           'euca2ools-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/euca2ools-3.1/centos/6/x86_64/'},
+                       'maint-4.1': {
+                           'eucalyptus-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/eucalyptus-4.1/centos/6/x86_64/',
+                           'enterprise-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/enterprise-4.1/centos/6/x86_64/',
+                           'euca2ools-repo': 'http://packages.release.eucalyptus-systems.com/yum/tags/euca2ools-3.2/centos/6/x86_64/'}
+}
+default = dict(default.items() + repostitory_mapping[euca_source].items())
 
 # Setup networking
 if 'EDGE' == network_mode:
-    config_json = {"InstanceDnsServers": ["frontend"],
+    config_json = {"InstanceDnsServers": ["CLC-IP"],
                    "Clusters": [{"Subnet": {"Subnet": "10.111.0.0",
                                             "Netmask": "255.255.0.0",
                                             "Name": "10.111.0.0",
@@ -68,5 +101,9 @@ if 'EDGE' == network_mode:
     eucalyptus['network']['nc-router'] = 'N'
     eucalyptus['network']['config-json'] = config_json
 
-print "Generated Environmentv\n"
+print "Generated Environment\n"
 print yaml.dump(merge(user_dict, default), default_flow_style=False)
+
+# topology_parser.get_topology()
+# topology_parser.print_topology()
+# topology_parser.set_number_hosts_to_reserve()
--- topology_parser.py
+++ topology_parser.py
@@ -2,19 +2,85 @@
 #
 # Author: Tony Beckham tony.beckham@hp.com
 #
-
 import os
 import yaml
 
 
-environment_descriptor= os.getenv('TOPOLOGY')
+# environment_descriptor= os.getenv('TOPOLOGY')
+# job_id= os.getenv('JOB_ID')
+
+#simulate jenkins env vars
+job_id= "tony-23"
+environment_descriptor='''
+version: 4.1
+machine-topology:
+  - cloud-components:
+    - clc
+  - cloud-components:
+    - cc
+    - sc
+    cluster-name: one
+  - cloud-components:
+    - nc
+    cluster-name: one
+  - cloud-components:
+    - nc
+    cluster-name: one
+  - cloud-components:
+    - walrus
+  - cloud-components:
+    - ufs
+    public-ip: 192.168.0.1
+'''
+
 topo_dict = yaml.load(environment_descriptor)
-num_hosts=len(topo_dict['machine-topology'])
-f = open('build.properties', 'w')
-f.write("NUM_HOSTS="+ str(num_hosts))
+client_dict={'default_attributes':
+                 {'eucalyptus':
+                      {'topology':
+                           {'clusters':
+                                {'one':
+                                     {'cc-1': 'CC-IP',
+                                      'nodes': 'NC-IP',
+                                      'sc-1': 'SC-IP'}},
+                            'clc-1': 'CLC-IP',
+                            'walrus': 'WALRUS-IP',
+                            'user-facing': ['UFS-IP']}}},
+             'name': job_id}
 
-print "\nThis is the determined topology"
-for i, v in enumerate(topo_dict['machine-topology']):
-    print v['cloud-components']
+def set_number_hosts_to_reserve():
+    num_hosts = 0
+    for machine in get_topology():
+        if not is_attribute_decalared(key="public-ip", dict=machine):
+            num_hosts += 1
+    f = open('build.properties', 'w')
+    f.write("NUM_HOSTS="+ str(num_hosts) + "\n")
+    f.close()
+    print "\nReserving " + str(num_hosts) + " hosts\n"
+    return
 
-print "\nReserving " + str(num_hosts) + " hosts\n"
+def get_topology():
+    machine_details = []
+    for i, v in enumerate(topo_dict['machine-topology']):
+        machine_details.append(topo_dict['machine-topology'][i])
+    return machine_details
+
+def print_topology():
+    print "\nThis is the determined topology"
+    for i, v in enumerate(topo_dict['machine-topology']):
+        print v['cloud-components']
+    return
+
+def is_attribute_decalared(key, dict):
+    if key in dict.keys():
+        return True
+    else:
+        return False
+
+def create_client_yml():
+    f = open('client.yml', 'w')
+    f.write(yaml.dump(client_dict))
+    f.close()
+
+print"\n"
+# for machine in get_topology():
+#     print machine
